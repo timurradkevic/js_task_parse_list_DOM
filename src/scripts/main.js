b6dada4cd860c784365822763e1a4734691517d5
Tymur <timurradkevic84@mail.com>
2026-04-06 15:33:58 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listEmploy = [...document.querySelectorAll('li')];
+
+function sortList(items) {
+  return items.sort((el1, el2) => {
+    const salary1 = Number(
+      el1.dataset.salary.replaceAll(',', '').replaceAll('$', ''),
+    );
+
+    const salary2 = Number(
+      el2.dataset.salary.replaceAll(',', '').replaceAll('$', ''),
+    );
+
+    return salary2 - salary1;
+  });
+}
+
+function toObjectEmployee(items) {
+  return items.reduce((res, el, i) => {
+    const obj = {};
+
+    obj.name = el.dataset.name;
+    obj.position = el.dataset.position;
+    obj.salary = el.dataset.salary;
+    obj.age = el.dataset.age;
+
+    res.push(obj);
+
+    return res;
+  }, []);
+}
+
+sortList(listEmploy);
+
+list.append(...listEmploy);
+
+toObjectEmployee(listEmploy);
